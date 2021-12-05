329fde228051087908083e4c5054127d6b4786f3
xpnobug <669307582@qq.com>
2021-12-05 13:48:40 +0800

    'image'

---
+++ uview-ui/components/u-loading/u-loading.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["uview-ui/components/u-loading/u-loading"],{"4a2e":function(e,t,n){"use strict";n.r(t);var r=n("df3a"),c=n.n(r);for(var a in r)"default"!==a&&function(e){n.d(t,e,(function(){return r[e]}))}(a);t["default"]=c.a},"516b":function(e,t,n){"use strict";var r;n.d(t,"b",(function(){return c})),n.d(t,"c",(function(){return a})),n.d(t,"a",(function(){return r}));var c=function(){var e=this,t=e.$createElement,n=(e._self._c,e.show?e.__get_style([e.cricleStyle]):null);e.$mp.data=Object.assign({},{$root:{s0:n}})},a=[]},"589a":function(e,t,n){"use strict";n.r(t);var r=n("516b"),c=n("4a2e");for(var a in c)"default"!==a&&function(e){n.d(t,e,(function(){return c[e]}))}(a);n("eba1");var o,u=n("f0c5"),i=Object(u["a"])(c["default"],r["b"],r["c"],!1,null,"631ca2f1",null,!1,r["a"],o);t["default"]=i.exports},b44d:function(e,t,n){},df3a:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var r={name:"u-loading",props:{mode:{type:String,default:"circle"},color:{type:String,default:"#c7c7c7"},size:{type:[String,Number],default:"34"},show:{type:Boolean,default:!0}},computed:{cricleStyle:function(){var e={};return e.width=this.size+"rpx",e.height=this.size+"rpx","circle"==this.mode&&(e.borderColor="#e4e4e4 #e4e4e4 #e4e4e4 ".concat(this.color?this.color:"#c7c7c7")),e}}};t.default=r},eba1:function(e,t,n){"use strict";var r=n("b44d"),c=n.n(r);c.a}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'uview-ui/components/u-loading/u-loading-create-component',
+    {
+        'uview-ui/components/u-loading/u-loading-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('a821')['createComponent'](__webpack_require__("589a"))
+        })
+    },
+    [['uview-ui/components/u-loading/u-loading-create-component']]
+]);
